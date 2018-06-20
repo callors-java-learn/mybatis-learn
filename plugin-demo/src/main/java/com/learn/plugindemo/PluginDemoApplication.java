82b9502a98996e3af72ba7ce15ce04a7a0f4bcff
callor-work <bingc420_work@qq.com>
2018-06-20 17:07:49 +0800

    edit .gitigore

--- plugin-demo/src/main/java/com/learn/plugindemo/PluginDemoApplication.java
+++ plugin-demo/src/main/java/com/learn/plugindemo/PluginDemoApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PluginDemoApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(PluginDemoApplication.class, args);
 	}
